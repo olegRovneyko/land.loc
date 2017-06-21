c8bb7ec8de1c025385c2107924e60415fa16b18c
Oleg Rovneyko <oleg_rov@ukr.net>
2017-06-21 13:42:17 +0300

    Создание моделей. Выборка информации из таблиц базы данных

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\Portfolio;
+use App\People;
+use App\Service;
+
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
+    	$pages = Page::all();
+    	$portfolios = Portfolio::all();
+    	$peoples = People::all();
+    	$services = Service::all();
 
-    	return view('layouts.site');
+    	return view('site.index');
     }
 }
